fe5d20728cf0a653390ddab80e1c7a4813c5d058
RikuH <riku_hei@hotmail.com>
2019-12-02 14:11:42 +0200

    Yeet

---
+++ Ball.py
@@ -0,0 +1,31 @@
+import pygame
+from random import randint
+
+BLACK = (0, 0, 0)
+
+class ball(pygame.sprite.Sprite):
+    def __init__(self, color, width, height, windowWidth, WindowHeight):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        self.velocity = [randint(6, 12), randint(-8, 8)]
+        #self.velocity = [randint(20, 40), randint(-80, 80)]
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = windowWidth / 2
+        self.rect.y = WindowHeight / 2
+
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def bounce(self):
+        self.velocity[0] = -self.velocity[0]
+        self.velocity[1] = randint(-8, 8)
+
---
+++ Paddle.py
@@ -0,0 +1,23 @@
+import pygame
+BLACK = (0, 0, 0)
+
+
+class paddle(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        self.rect = self.image.get_rect()
+
+    def moveUp(self, pixels):
+        if self.rect.y > 5:
+            self.rect.y -= pixels
+
+    def moveDown(self, pixels, windowHeight):
+        if self.rect.y <= windowHeight - 60:
+            self.rect.y += pixels          
